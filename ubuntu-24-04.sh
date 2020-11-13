836c90b47c633eb4689292b2e8618dbb6a02ac84
Hugo <info@nygts.com>
2020-11-13 10:25:43 -0500

    init

---
+++ ubuntu-24-04.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+echo "Rails installation"
+
+echo "adding packages sources"
+sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+echo "updating and installing dependencies"
+sudo apt-get update -y
+sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn -y
+
+echo "installing rbenv"
+cd
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+exec $SHELL
+
+rbenv install 2.7.2
+rbenv global 2.7.2
+ruby -v
+
+echo "installing bundler"
+gem install bundler
+
+echo "installing rails"
+gem install rails -v 6.0.3.4
+
+echo "installing PostgreSQL"
+sudo apt install postgresql-11 libpq-dev
